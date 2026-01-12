7073aa38c8e85391f5c140990b2913c2024c6683
Matheus Passos <passosmatheus.work@gmail.com>
2026-01-12 17:04:51 -0300

    feat: init logic.

--- inference.py
+++ inference.py
@@ -3,6 +3,51 @@ import torch
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-pass
+model_path: str = os.getenv("MODEL_PATH")
 
-pass
+tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
+
+model = AutoModelForCausalLM.from_pretrained(
+            model_path,
+            local_files_only=True,
+            trust_remote_code=True
+        )
+
+device = torch.device("cpu")
+model.to(device)
+
+model.eval()
+
+system_prompt = "você é um assistente legal."
+
+TOOLS = []
+
+def run_inference(input: str, max_tokens: int = 128) -> str:
+
+    messages = []
+
+    inputs = tokenizer.apply_chat_template(
+                messages,
+                tools=TOOLS,
+                add_generation_prompt=True,
+                return_dict=True,
+                return_tensors="pt"
+            )
+
+    with torch.no_grad():
+        outputs = model.generate(
+                    **inputs.to(device),
+                    max_new_tokens=max_tokens,
+                    pad_token_id=tokenizer.eos_token_id
+                )
+
+        generated = tokenizer.decode(
+                    outputs[0][len(inputs["inputs_ids"][0]):],
+                    skip_special_tokens=False
+                )
+
+        return generated
+
+
+print(run_inference("30 reais débito"))
+
